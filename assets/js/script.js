ead41432269b38a79a073b580910c821657d17e2
jceyang <jceyang@Jonathans-MacBook-Pro-2.local>
2017-04-22 12:41:41 -0700

    awef

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    $('#wrapper').fadeIn(1500);
+  $('#wrapper').fadeIn(1500);
+
+  $("#ashaTag").click(function(){
+    $("#placeTitle").fadeOut("1000");
+	});
+
+  $("#ashaTag").click(function(){
+    $('#asha').delay(250).fadeIn(1500);
+    console.log("hi")
+  });
+
+  $("#pagewrapper").click(function(){
+    $("#asha").fadeOut(1000);
+    console.log("hi");
+	});
+  
 });
